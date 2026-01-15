eed84b0462d5712da3f3881962ff624d00319f40
Sebastian Hansen <Sebastian.hansen@dataalchemy.dev>
2026-01-15 13:31:09 -0800

    testing workspace creation with newly created capacity

---
+++ build/artifacts/temp_workspace.tf
@@ -0,0 +1,19 @@
+module "fabws_test41_i1_er" {
+  source = "../modules/azure/fabric_workspace/"
+  providers = {
+    fabric.auth = fabric.auth
+  }
+  project                     = "fabric"
+  prefix                      = "nr"
+  instance_number             = 1
+  description                 = "terraform test workspace"
+  capacity_id                 = "77e89824-a07e-42b8-b47f-2c68c13fb559"
+  identity_type               = "SystemAssigned"
+  enable_git_integration      = false
+  git_initialization_strategy = null
+  git_provider_details        = null
+  timeouts                    = {}
+  timeouts_git                = {}
+  owners                      = ["4252e715-8be7-4d78-bfd7-6ec61a981444"]
+}
+
