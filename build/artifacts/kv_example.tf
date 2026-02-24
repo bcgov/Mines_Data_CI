96f191c5b5dbe425dafaef47a227e0749caa908a
Sebastian Hansen <Sebastian.hansen@dataalchemy.dev>
2026-02-24 11:33:34 -0800

    testing kv with what i found in the policy

--- build/artifacts/kv_example.tf
+++ build/artifacts/kv_example.tf
@@ -1,44 +1,43 @@
-# module "key_vault" {
-#   source = "../modules/azure/key_vaults"
+module "key_vault" {
+  source = "../modules/azure/key_vaults"
 
-#   prefix              = "nr"
-#   suffix              = "tools"
-#   project             = "fabric"
-#   env                 = "dev"
-#   Instance_Number     = "01"
-#   resource_group_name = "rg-nr-tools-dev-01"
-#   location            = "canadacentral"
+  prefix              = "nr"
+  suffix              = "tools"
+  project             = "fabric"
+  env                 = "dev"
+  Instance_Number     = "01"
+  resource_group_name = "rg-nr-tools-dev-01"
+  location            = "canadacentral"
 
-#   public_network_access_enabled = false
-#   private_endpoint_enabled      = true
-#   private_endpoint_subnet_id    = module.vnet.subnet_ids["snet-pe"]
-#   private_dns_zone_ids          = [module.vnet.kv_private_dns_zone_id]
-#   virtual_network_subnet_ids    = [module.vnet.subnet_ids["snet-app"]]
+  public_network_access_enabled = false
+  private_endpoint_enabled      = true
+  private_endpoint_subnet_id    = "/subscriptions/ffc5e617-7f2d-4ddb-8b57-33fc43989a8c/resourceGroups/b9cee3-tools-networking/providers/Microsoft.Network/virtualNetworks/b9cee3-tools-vwan-spoke/subnets/privateendpoints-subnet"
+  private_dns_zone_ids          = ["/subscriptions/ffc5e617-7f2d-4ddb-8b57-33fc43989a8c/resourceGroups/b9cee3-tools-networking/providers/Microsoft.Network/virtualNetworks/b9cee3-tools-vwan-spoke/subnets/privateendpoints-subnet"]
 
-#   additional_access_policies = [
-#     {
-#       object_id = "6f523291-c78a-4e05-aae1-de6b07a07a11"
-#       key_permissions = [
-#         "Create",
-#         "Decrypt",
-#         "Encrypt",
-#         "Get",
-#         "List",
-#         "Rotate",
-#         "GetRotationPolicy",
-#         "SetRotationPolicy",
-#       ]
-#       secret_permissions = [
-#         "Get",
-#         "List",
-#         "Set",
-#         "Delete",
-#         "Recover",
-#         "Backup",
-#         "Restore",
-#       ]
-#     }
-#   ]
+  additional_access_policies = [
+    {
+      object_id = "6f523291-c78a-4e05-aae1-de6b07a07a11"
+      key_permissions = [
+        "Create",
+        "Decrypt",
+        "Encrypt",
+        "Get",
+        "List",
+        "Rotate",
+        "GetRotationPolicy",
+        "SetRotationPolicy",
+      ]
+      secret_permissions = [
+        "Get",
+        "List",
+        "Set",
+        "Delete",
+        "Recover",
+        "Backup",
+        "Restore",
+      ]
+    }
+  ]
 
-#   depends_on = [module.vnet]
-# }
+  depends_on = [module.vnet]
+}
