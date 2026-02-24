8e6159cfee5a69c0a4c2f98a657e076274b29b2a
Sebastian Hansen <Sebastian.hansen@dataalchemy.dev>
2026-02-24 11:21:58 -0800

    removing kv until i can get the details from Abi

--- build/artifacts/kv_example.tf
+++ build/artifacts/kv_example.tf
@@ -1,44 +1,44 @@
-module "key_vault" {
-  source = "../modules/azure/key_vaults"
+# module "key_vault" {
+#   source = "../modules/azure/key_vaults"
 
-  prefix              = "nr"
-  suffix              = "tools"
-  project             = "fabric"
-  env                 = "dev"
-  Instance_Number     = "01"
-  resource_group_name = "rg-nr-tools-dev-01"
-  location            = "canadacentral"
+#   prefix              = "nr"
+#   suffix              = "tools"
+#   project             = "fabric"
+#   env                 = "dev"
+#   Instance_Number     = "01"
+#   resource_group_name = "rg-nr-tools-dev-01"
+#   location            = "canadacentral"
 
-  public_network_access_enabled = false
-  private_endpoint_enabled      = true
-  private_endpoint_subnet_id    = module.vnet.subnet_ids["snet-pe"]
-  private_dns_zone_ids          = [module.vnet.kv_private_dns_zone_id]
-  virtual_network_subnet_ids    = [module.vnet.subnet_ids["snet-app"]]
+#   public_network_access_enabled = false
+#   private_endpoint_enabled      = true
+#   private_endpoint_subnet_id    = module.vnet.subnet_ids["snet-pe"]
+#   private_dns_zone_ids          = [module.vnet.kv_private_dns_zone_id]
+#   virtual_network_subnet_ids    = [module.vnet.subnet_ids["snet-app"]]
 
-  additional_access_policies = [
-    {
-      object_id = "6f523291-c78a-4e05-aae1-de6b07a07a11"
-      key_permissions = [
-        "Create",
-        "Decrypt",
-        "Encrypt",
-        "Get",
-        "List",
-        "Rotate",
-        "GetRotationPolicy",
-        "SetRotationPolicy",
-      ]
-      secret_permissions = [
-        "Get",
-        "List",
-        "Set",
-        "Delete",
-        "Recover",
-        "Backup",
-        "Restore",
-      ]
-    }
-  ]
+#   additional_access_policies = [
+#     {
+#       object_id = "6f523291-c78a-4e05-aae1-de6b07a07a11"
+#       key_permissions = [
+#         "Create",
+#         "Decrypt",
+#         "Encrypt",
+#         "Get",
+#         "List",
+#         "Rotate",
+#         "GetRotationPolicy",
+#         "SetRotationPolicy",
+#       ]
+#       secret_permissions = [
+#         "Get",
+#         "List",
+#         "Set",
+#         "Delete",
+#         "Recover",
+#         "Backup",
+#         "Restore",
+#       ]
+#     }
+#   ]
 
-  depends_on = [module.vnet]
-}
+#   depends_on = [module.vnet]
+# }
--- build/artifacts/vnet_example.tf
+++ build/artifacts/vnet_example.tf
@@ -1,23 +1,23 @@
-module "vnet" {
-  source = "../modules/azure/vnet"
+# module "vnet" {
+#   source = "../modules/azure/vnet"
 
-  prefix          = "nr"
-  suffix          = "tools"
-  project         = "fabric"
-  env             = "dev"
-  instance_number = "01"
+#   prefix          = "nr"
+#   suffix          = "tools"
+#   project         = "fabric"
+#   env             = "dev"
+#   instance_number = "01"
 
-  resource_group_name = "rg-nr-tools-dev-01"
-  location            = "canadacentral"
-  address_space       = ["10.0.0.0/16"]
+#   resource_group_name = "rg-nr-tools-dev-01"
+#   location            = "canadacentral"
+#   address_space       = ["10.0.0.0/16"]
 
-  subnets = {
-    snet-app = {
-      address_prefixes = ["10.0.1.0/24"]
-    }
-    snet-pe = {
-      address_prefixes                  = ["10.0.2.0/24"]
-      private_endpoint_network_policies = "Disabled"
-    }
-  }
-}
+#   subnets = {
+#     snet-app = {
+#       address_prefixes = ["10.0.1.0/24"]
+#     }
+#     snet-pe = {
+#       address_prefixes                  = ["10.0.2.0/24"]
+#       private_endpoint_network_policies = "Disabled"
+#     }
+#   }
+# }
