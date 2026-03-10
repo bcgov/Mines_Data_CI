1f8d119f0041014f41096e6eadae27cb5ace9b7c
Sebastian Hansen <shansen@lululemon.com>
2026-03-10 15:06:45 -0700

    adding dynamic environment variable for kv example

--- build/artifacts/kv_example.tf
+++ build/artifacts/kv_example.tf
@@ -4,7 +4,7 @@ module "key_vault" {
   prefix              = "nr"
   suffix              = "tools"
   project             = "fabric"
-  env                 = "dev"
+  env                 = var.ENVIRONMENT 
   Instance_Number     = "01"
   resource_group_name = "nrfabric-rg1"
   location            = "canadacentral"
