20fa428dfab49dec3680535f4ba5b491c36fd6bf
Sebastian Hansen <shansen@lululemon.com>
2026-03-12 14:56:46 -0700

    updating the cd pipeline to always commit the statefile even on error, raises issue with partial failure warning

--- build/artifacts/kv_example.tf
+++ build/artifacts/kv_example.tf
@@ -15,7 +15,7 @@ module "key_vault" {
   location            = "canadacentral"
 
   public_network_access_enabled = false
-  private_endpoint_enabled      = true
+  private_endpoint_enabled      = false
   private_endpoint_subnet_id    = "/subscriptions/ffc5e617-7f2d-4ddb-8b57-33fc43989a8c/resourceGroups/b9cee3-tools-networking/providers/Microsoft.Network/virtualNetworks/b9cee3-tools-vwan-spoke/subnets/privateendpoints-subnet"
   private_dns_zone_ids          = []
 }
