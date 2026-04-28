a2bebece6a09d95a42400ad0def279286ad7b3ac
Sebastian Hansen <shansen@lululemon.com>
2026-04-28 08:11:14 -0700

    feature: initial config dev mines environment, removing additional rg as its not needed

--- build/artifacts/fabric_capacities.tf
+++ build/artifacts/fabric_capacities.tf
@@ -5,7 +5,6 @@ module "fabric_capacity" {
   project             = "fabric"
   Instance_Number     = "01"
   location            = "canadacentral"
-  resource_group_name = module.resource_group_mines.rg_name
   Owners              = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
 }
 
