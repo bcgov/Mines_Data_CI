a3c9517339ee4a7ecb554380f51fe2524b24a601
Sebastian Hansen <shansen@lululemon.com>
2026-04-28 09:18:17 -0700

    feat: adding dev workspace removing new capacity

--- build/artifacts/fabric_workspace.tf
+++ build/artifacts/fabric_workspace.tf
@@ -14,3 +14,19 @@ module "fabric_workspace" {
   owners          = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
   identity_type   = "SystemAssigned"
 }
+
+
+module "fabric_workspace2" {
+  source = "../modules/azure/fabric_workspace"
+
+  providers = {
+    fabric.auth = fabric.auth
+  }
+
+  prefix          = "mines"
+  project         = "fabric"
+  instance_number = "02"
+  capacity_id     = "198C68F4-8402-45B9-8010-BDE58A729DDF"
+  owners          = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
+  identity_type   = "SystemAssigned"
+}
