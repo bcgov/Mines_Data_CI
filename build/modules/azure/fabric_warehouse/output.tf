41402185a11510eaa52e707d53cddbbf75d24bc2
Sebastian Hansen <shansen@lululemon.com>
2026-04-28 11:36:32 -0700

    adding warehouse and lakehouse for the workspace

--- build/modules/azure/fabric_warehouse/output.tf
+++ build/modules/azure/fabric_warehouse/output.tf
@@ -1,8 +1,12 @@
-output "warehouse_id" {
-  value = resource.fabric_warehouse.this_warehouse
+output "warehouse" {
+  value = fabric_warehouse.this_warehouse
 }
 
-
 output "warehouse_id" {
-  value = resource.fabric_warehouse.this_warehouse.id
+  value = fabric_warehouse.this_warehouse.id
+}
+
+output "warehouse_connection_string" {
+  description = "The SQL connection string for the warehouse."
+  value       = fabric_warehouse.this_warehouse.connection_string
 }
