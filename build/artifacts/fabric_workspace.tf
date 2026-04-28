5011a84f176f0229617ccfdfe20f20c009e77af4
Sebastian Hansen <shansen@lululemon.com>
2026-04-28 09:29:39 -0700

    renaming workspace

--- build/artifacts/fabric_workspace.tf
+++ build/artifacts/fabric_workspace.tf
@@ -8,7 +8,7 @@ module "fabric_workspace" {
   }
 
   prefix          = "mines"
-  project         = "fabric"
+  project         = "data-platform"
   instance_number = "01"
   capacity_id     = "198C68F4-8402-45B9-8010-BDE58A729DDF"
   owners          = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
