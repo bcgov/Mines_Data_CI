7e46dac70bc47d325efbfa3435398e1957c23059
Sebastian Hansen <shansen@lululemon.com>
2026-04-27 21:10:27 -0700

    feature: initial config dev mines environment

--- build/artifacts/fabric_rg.tf
+++ build/artifacts/fabric_rg.tf
@@ -10,15 +10,3 @@ module "resource_group_mines" {
   Instance_Number = "01"
   location        = "canadacentral"
 }
-
-module "fabric_capacity" {
-  source = "../modules/azures/fabric_rm_capacity"
-  prefix                = "mines"
-  suffix                = var.ENVIRONMENT
-  project               = "fabric"
-  Instance_Number       = "01"
-  location              = "canadacentral"
-  resource_group_name   = module.resource_group.rg_name
-  Owners                = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
-  depends_on = [ module.resource_group_mines ]
-}
