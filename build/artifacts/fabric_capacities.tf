04445269e472f82b273e831a6412cca03c2ca6eb
Sebastian Hansen <shansen@lululemon.com>
2026-04-28 07:47:05 -0700

    feature: initial config dev mines environment, removing dependency

--- build/artifacts/fabric_capacities.tf
+++ build/artifacts/fabric_capacities.tf
@@ -7,5 +7,4 @@ module "fabric_capacity" {
   location              = "canadacentral"
   resource_group_name   = module.resource_group.rg_name
   Owners                = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
-  depends_on = [ module.resource_group_mines ]
 }
