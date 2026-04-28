7983bd4d8f3fb116edb2eb6ad91a04736d85c222
Sebastian Hansen <shansen@lululemon.com>
2026-04-28 12:31:48 -0700

    remove all and rebuild

--- build/artifacts/fabric_warehouse.tf
+++ build/artifacts/fabric_warehouse.tf
@@ -1,11 +1,11 @@
-# module "fabric_warehouse" {
-#   source = "../modules/azure/fabric_warehouse"
+module "fabric_warehouse" {
+  source = "../modules/azure/fabric_warehouse"
 
-#   providers = {
-#     fabric.auth = fabric.auth
-#   }
-#   prefix          = "mines"
-#   project         = "data-platform"
-#   instance_number = 01
-#   workspace_id    = module.fabric_workspace.workspace_id
-# }
+  providers = {
+    fabric.auth = fabric.auth
+  }
+  prefix          = "mines"
+  project         = "data-platform"
+  instance_number = 01
+  workspace_id    = module.fabric_workspace.workspace_id
+}
--- build/artifacts/fabric_workspace.tf
+++ build/artifacts/fabric_workspace.tf
@@ -1,16 +1,16 @@
-# # artifacts/fabric_workspace.tf
+# artifacts/fabric_workspace.tf
 
-# module "fabric_workspace" {
-#   source = "../modules/azure/fabric_workspace"
+module "fabric_workspace" {
+  source = "../modules/azure/fabric_workspace"
 
-#   providers = {
-#     fabric.auth = fabric.auth
-#   }
-
-#   prefix          = "mines"
-#   project         = "data-platform"
-#   instance_number = "01"
-#   capacity_id     = "198C68F4-8402-45B9-8010-BDE58A729DDF"
-#   owners          = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
-#   identity_type   = "SystemAssigned"
-# }
+  providers = {
+    fabric.auth = fabric.auth
+  }
+  env             = var.ENVIRONMENT
+  prefix          = "mines"
+  project         = "data-platform"
+  instance_number = "01"
+  capacity_id     = "198C68F4-8402-45B9-8010-BDE58A729DDF"
+  owners          = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
+  identity_type   = "SystemAssigned"
+}
