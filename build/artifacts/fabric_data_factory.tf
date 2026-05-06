8a022bbfd819b967f8d2b894325ef6ed47ff2983
Sebastian Hansen <shansen@lululemon.com>
2026-05-06 08:27:06 -0700

    updating adf code to mount adf to fabric properly

--- build/artifacts/fabric_data_factory.tf
+++ build/artifacts/fabric_data_factory.tf
@@ -30,7 +30,7 @@ module "key_vault_adf" {
   sku_name                        = "standard"
   enabled_for_disk_encryption     = false
   enabled_for_template_deployment = true
-  soft_delete_retention_days      = 90
+  soft_delete_retention_days      = 7
   purge_protection_enabled        = true
   public_network_access_enabled   = false
   enable_rbac_assignments         = true
