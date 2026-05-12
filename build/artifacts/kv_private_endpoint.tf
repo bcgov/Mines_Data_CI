6aad555f21c9b52abdafca4b79816fdac4e633b4
Sebastian Hansen <shansen@lululemon.com>
2026-05-12 12:43:16 -0700

    adding private endpoint for keyvault access

--- build/artifacts/kv_private_endpoint.tf
+++ build/artifacts/kv_private_endpoint.tf
@@ -31,34 +31,3 @@ output "kv_private_endpoint_ip" {
   description = "Private IP of the Key Vault private endpoint."
   value       = azurerm_private_endpoint.kv.private_service_connection[0].private_ip_address
 }
-
-###############################################################################
-# Inject hosts entry into tunnel container
-#
-# Runs after the PE is created. Writes the KV private IP to /etc/hosts
-# inside the running tunnel container so vault.azure.net resolves via
-# the private endpoint without needing a DNS zone.
-#
-# Re-runs whenever the private IP changes (e.g. PE recreated).
-###############################################################################
-
-resource "terraform_data" "kv_hosts_entry" {
-  input = azurerm_private_endpoint.kv.private_service_connection[0].private_ip_address
-
-  provisioner "local-exec" {
-    command = join(" ", [
-      "az container exec",
-      "--subscription 53205a1b-0f8d-459e-a424-65f1b39ec648",
-      "--resource-group ${module.resource_group_adf.rg_name}",
-      "--name mines-fabric-tunnel01",
-      "--container-name vscode-tunnel",
-      "--exec-command",
-      "\"sh -c 'echo ${azurerm_private_endpoint.kv.private_service_connection[0].private_ip_address} mines-fabric-kv01.vault.azure.net >> /etc/hosts && echo hosts entry added'\""
-    ])
-  }
-
-  depends_on = [
-    azurerm_private_endpoint.kv,
-    module.vscode_tunnel,
-  ]
-}
