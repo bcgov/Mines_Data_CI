5cf1d6d080fbec10a7849b65ce354a0044d428c8
Sebastian Hansen <shansen@lululemon.com>
2026-08-24 11:20:53 -0700

    adding Nhung and Raj to owernship group

--- build/artifacts/fabric_core_datasource.tf
+++ build/artifacts/fabric_core_datasource.tf
@@ -1,19 +1,19 @@
-module "postgres_mds_test_connection" {
-  source = "../modules/azure/fabric_connection"
+# module "postgres_mds_test_connection" {
+#   source = "../modules/azure/fabric_connection"
 
-  display_name      = "postgres-mds-test"
-  connection_type   = "PostgreSQL"
-  connectivity_type = "VirtualNetworkGateway"
-  gateway_id        = "f15b9af0-2bda-4add-85a1-066d89935a82"
+#   display_name      = "postgres-mds-test"
+#   connection_type   = "PostgreSQL"
+#   connectivity_type = "VirtualNetworkGateway"
+#   gateway_id        = "f15b9af0-2bda-4add-85a1-066d89935a82"
 
-  server   = "142.34.194.69:48625"
-  database = "mds"
-  username = "mds_data_analytics"
-  password = var.mds_test_password
-  owner_principal_ids = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
-}
+#   server   = "142.34.194.69:48625"
+#   database = "mds"
+#   username = "mds_data_analytics"
+#   password = var.mds_test_password
+#   owner_principal_ids = ["b0bf68e8-4e08-433c-8903-19b2fec4cc20"]
+# }
 
-output "postgres_mds_test_connection_id" {
-  description = "Pass this into pipeline modules as source_connection_id for MDS Core Test."
-  value       = module.postgres_mds_test_connection.connection_id
-}
+# output "postgres_mds_test_connection_id" {
+#   description = "Pass this into pipeline modules as source_connection_id for MDS Core Test."
+#   value       = module.postgres_mds_test_connection.connection_id
+# }
