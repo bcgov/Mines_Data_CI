7581b9df775ba5aac1ccd0800940ed88fa98a077
Sebastian Hansen <shansen@lululemon.com>
2026-09-16 15:33:38 -0700

    adding prod configurations for pipelines

--- build/artifacts/fabric_workspace.tf
+++ build/artifacts/fabric_workspace.tf
@@ -8,7 +8,7 @@ module "fabric_workspace_01" {
   env             = var.ENVIRONMENT
   prefix          = var.PREFIX
   project         = var.PROJECT
-  instance_number = "01"
+  instance_number = "00"
   capacity_id     = local.fabric_capacity_id
   owners          = var.WORKSPACE_OWNERS
   identity_type   = "SystemAssigned"
